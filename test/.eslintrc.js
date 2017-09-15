4bc730501ebaf739f54f1f752b629c0204588669
Joerg Boeselt <sendstuff2bo@gmail.com>
2017-09-15 22:44:08 +0930

    eslintrc for tests

---
+++ test/.eslintrc.js
@@ -0,0 +1,7 @@
+// modified version of https://github.com/angular/angular.js/blob/master/.eslintrc-base.json
+module.exports = {
+    "extends": "../.eslintrc-base.js",
+    "rules": {
+        "prefer-const": "error"
+    }
+};
