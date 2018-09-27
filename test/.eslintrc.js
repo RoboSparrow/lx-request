c0625c656b35b651d09aa2074dd6a2213432cee5
joerg <sendstuff2bo@gmail.com>
2018-09-27 13:48:16 +0930

    linting, es6

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,9 +1,9 @@
 // modified version of https://github.com/angular/angular.js/blob/master/.eslintrc-base.json
 module.exports = {
     "extends": "../.eslintrc-base.js",
-    "env": {
-        "es6": false
-    },
+    // "env": {
+    //     "es6": false
+    // },
     "rules": {
         "prefer-const": "error"
     }
